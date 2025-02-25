4b7e297e3a492c553f8cf9c87acbf174fd3c323e
Carla <carlajimenamolina@gmail.com>
2025-02-24 23:56:11 -0300

    muchisimas cosas

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,3 +260,17 @@
 			});
 
 })(jQuery);
+
+
+
+let index = 0;
+const carousel = document.querySelector('.carousel');
+const images = document.querySelectorAll('.carousel img');
+
+function changeImage() {
+    images[index].classList.remove('active');
+    index = (index + 1) % images.length;
+    images[index].classList.add('active');
+}
+
+setInterval(changeImage, 4000);
